93f3dc5e1f1c393d92f826a765d7d1f48933e6ff
Manjunadhganta <gantamanjunadhreddy@gmail.com>
2025-06-22 23:32:51 +0530

    About & Features added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import About from './components/About'
-import Price from './components/Price'
+// import Price from './components/Price'
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
       <Hero />
       <About />
       <Features />
-      <Price />
+
     </>
   )
 }
